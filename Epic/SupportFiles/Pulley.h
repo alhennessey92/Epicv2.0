5be45030244a73fcc959546f453d213b46340425
Al Hennessey <alhennessey92@gmail.com>
2019-03-15 19:12:20 +0000

    v2.1

---
+++ Epic/SupportFiles/Pulley.h
@@ -0,0 +1,17 @@
+//
+//  Pulley.h
+//  Epic
+//
+//  Created by Al Hennessey on 11/03/2019.
+//  Copyright © 2019 Epic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Pulley.
+FOUNDATION_EXPORT double PulleyVersionNumber;
+
+//! Project version string for Pulley.
+FOUNDATION_EXPORT const unsigned char PulleyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pulley/PublicHeader.h>
